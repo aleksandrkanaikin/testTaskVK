2b0dd4c97de2ed41de57269d9ef3b1cfe5d45eda
aleksandrkanaikin <aleksandrkanajkin7@gmail.com>
2024-03-11 23:01:59 +0300

    Initial commit

---
+++ src/main/java/org/example/testtaskvk/TestTaskVkApplication.java
@@ -0,0 +1,13 @@
+package org.example.testtaskvk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskVkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskVkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/testtaskvk/TestTaskVkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.testtaskvk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskVkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
